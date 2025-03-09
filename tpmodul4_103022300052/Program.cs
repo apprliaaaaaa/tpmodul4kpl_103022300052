af2d3c1da24f6de94867c2713ca38e1a35000bbe
apprliaaaaaa <99806430+apprliaaaaaa@users.noreply.github.com>
2025-03-09 15:42:13 +0700

    Menambahkan implementasi dengan state-based constuction

--- tpmodul4_103022300052/Program.cs
+++ tpmodul4_103022300052/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul4_103022300052;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Kode Pos Batununggal: " + KodePos.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Kujangsari: " + KodePos.GetKodePos("Kujangsari"));
+
+        Console.WriteLine("===== SIMULASI DOOR MACHINE =====\n");
+
+        DoorMachine pintu = new DoorMachine();
+
+        Console.WriteLine(">>> Membuka pintu...");
+        pintu.BukaPintu(); 
+
+        Console.WriteLine(">>> Mengunci pintu...");
+        pintu.KunciPintu(); 
+
+        Console.WriteLine(">>> Membuka pintu lagi...");
+        pintu.BukaPintu(); 
+
+        Console.WriteLine(">>> Mencoba membuka pintu yang sudah terbuka...");
+        pintu.BukaPintu(); 
+
+        Console.WriteLine(">>> Mengunci pintu...");
+        pintu.KunciPintu(); 
+        Console.WriteLine(">>> Mencoba mengunci pintu yang sudah terkunci...");
+        pintu.KunciPintu(); 
+
+        Console.WriteLine("\n===== SIMULASI SELESAI =====");
+        Console.WriteLine("\nTekan Enter untuk keluar...");
+        Console.ReadLine();
+    }
+}
+
